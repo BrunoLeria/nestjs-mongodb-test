3e0b00c7d3f20974c333939e470e59c77e52d2d0
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-13 09:23:36 -0300

    Add AppController, AppService and UserSchema to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserSchema } from './schema/user.schema';
 
 // eslint-disable-next-line prettier/prettier
 @Module({
@@ -7,6 +10,9 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot('mongodb://localhost:27017', {
       dbName: 'userdb',
     }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

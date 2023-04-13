08edf23e535e97802473033c3a4c629ee59e66ad
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-13 09:33:50 -0300

    Add class-transformer and class-validator to package.json, and create CreateUserDto with email, first_name, last_name, and avatar fields, each with their own validation rules

---
+++ src/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsEmail, IsString, MaxLength } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+  @IsString()
+  @MaxLength(30)
+  @IsNotEmpty()
+  readonly first_name: string;
+  @IsString()
+  @MaxLength(30)
+  @IsNotEmpty()
+  readonly last_name: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly avatar: string;
+}

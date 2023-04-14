d9820d852e4ad70c49274883598166639be8f7db
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-14 14:19:54 -0300

    Add nodemailer and NestJS mailer module to send welcome email to new users

--- src/service/user/user.service.ts
+++ src/service/user/user.service.ts
@@ -3,11 +3,24 @@ import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from 'src/dto/create-user.dto';
 import { IUser } from 'src/interface/user.interface';
 import { Model } from 'mongoose';
+import { MailerService } from '@nestjs-modules/mailer';
 @Injectable()
 export class UserService {
-  constructor(@InjectModel('User') private userModel: Model<IUser>) {}
+  constructor(
+    @InjectModel('User') private userModel: Model<IUser>,
+    private mailerService: MailerService,
+  ) {}
   async createUser(createUserDto: CreateUserDto): Promise<IUser> {
     const newUser = new this.userModel(createUserDto);
-    return newUser.save();
+    newUser.save();
+    const message = `Hi ${newUser.first_name}, welcome to our platform!`;
+    await this.mailerService.sendMail({
+      to: newUser.email,
+      from: 'wesley.gado@treinaweb.com.br',
+      subject: 'Enviando Email com NestJS',
+      html: `<h3 style="color: red">${message}</h3>`,
+    });
+
+    return newUser;
   }
 }

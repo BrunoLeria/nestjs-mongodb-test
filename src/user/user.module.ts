d60d73ead2382f879965067d89d159e6a92d07ef
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-09 03:17:52 -0300

    Add HttpModule and AvatarModule imports to user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,17 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
-import { EmailModule } from 'src/email/email.module';
+import { EmailModule } from '../email/email.module';
 import { UserRepository } from './user.repository';
+import { HttpModule } from '@nestjs/axios';
+import { AvatarModule } from '../avatar/avatar.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     EmailModule,
+    HttpModule,
+    AvatarModule,
   ],
   controllers: [UserController],
   providers: [UserService, UserRepository],

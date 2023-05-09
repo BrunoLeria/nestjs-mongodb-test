9b9fc95ae0c665b0a07712b04219fa49c239deb9
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-09 02:00:39 -0300

    Delete avatar interface and schema files

--- src/external-api/interface/avatar.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Document } from 'mongoose';
-export interface IAvatar extends Document {
-  readonly userId: string;
-  readonly file: string;
-}
--- src/external-api/schema/avatar.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-@Schema()
-export class Avatar {
-  @Prop()
-  userId: string;
-  @Prop()
-  file: string;
-}
-export const AvatarSchema = SchemaFactory.createForClass(Avatar);

69fbcd0f99148b535f8c6102465a28c029b21b38
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-09 03:17:59 -0300

    Add e2e test for creating a user in UsersController

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { User } from '../src/user/schema/user.schema';
+import { AppModule } from '../src/app.module';
+import { CreateUserDto } from '../src/user/dto/create-user.dto';
+import { UpdateUserDto } from '../src/user/dto/update-user.dto';
+
+describe('UsersController (e2e)', () => {
+  let app: INestApplication;
+  let createUserDto: CreateUserDto;
+  let updateUserDto: UpdateUserDto;
+  let user: User;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    createUserDto = {
+      email: 'lindsay.ferguson@reqres.in',
+      first_name: 'Lindsay',
+      last_name: 'Ferguson',
+    };
+    updateUserDto = {
+      email: 'mark.ferguson@reqres.in',
+      first_name: 'Mark',
+      last_name: 'Ferguson',
+    };
+    await app.init();
+  });
+
+  describe('it should create a user', () => {
+    it('/users (POST)', async () => {
+      return request(app.getHttpServer())
+        .post('/users')
+        .send(createUserDto)
+        .expect(201)
+        .expect((res) => {
+          user = res.body;
+          expect(user).toHaveProperty('userId');
+          expect(user).toHaveProperty('email', createUserDto.email);
+          expect(user).toHaveProperty('first_name', createUserDto.first_name);
+          expect(user).toHaveProperty('last_name', createUserDto.last_name);
+        });
+    });
+  });
+});

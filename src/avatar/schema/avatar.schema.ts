69a1677bc6152975701ba6998c04fc7272c5dd16
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-09 02:01:07 -0300

    Add Avatar schema for Mongoose ODM

---
+++ src/avatar/schema/avatar.schema.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { AbstractDocument } from '../../database/abstract.schema';
+
+@Schema()
+export class Avatar extends AbstractDocument {
+  @Prop()
+  avatarId: string;
+  @Prop()
+  userId: string;
+  @Prop()
+  file: string;
+}
+export const AvatarSchema = SchemaFactory.createForClass(Avatar);

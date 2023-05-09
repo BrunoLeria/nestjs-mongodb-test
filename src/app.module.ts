09cd8e91d5abb9199e96ff4c38e1d4f69c59c568
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-09 02:03:17 -0300

    Add configuration module and email module to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,27 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './user/schema/user.schema';
 import { HttpModule } from '@nestjs/axios';
-import { AvatarSchema } from './external-api/schema/avatar.schema';
 import { UserModule } from './user/user.module';
-import { ExternalApiModule } from './external-api/external-api.module';
+import { AvatarModule } from './avatar/avatar.module';
+import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from './email/email.module';
+import * as Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
 
 // eslint-disable-next-line prettier/prettier
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'userdb',
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        EMAIL_SERVICE: Joi.string().required(),
+        EMAIL_USER: Joi.string().required(),
+        EMAIL_PASSWORD: Joi.string().required(),
+      }),
     }),
-    MongooseModule.forFeature([
-      { name: 'User', schema: UserSchema },
-      { name: 'Avatar', schema: AvatarSchema },
-    ]),
+    DatabaseModule,
     HttpModule,
     UserModule,
-    ExternalApiModule,
+    AvatarModule,
+    EmailModule,
   ],
 })
 export class AppModule {}

4c229810ca5f0eadc73dd696ec1f3dc1c13727aa
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-10 16:06:23 -0300

    Add ConfigModule to EmailService test file

--- src/email/test/email.service.spec.ts
+++ src/email/test/email.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import EmailService from '../email.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('EmailService', () => {
   let service: EmailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       providers: [EmailService],
     }).compile();
 
@@ -15,4 +17,19 @@ describe('EmailService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // describe('sendEmail', () => {
+  //   it('should send an email', async () => {
+  //     const options = {
+  //       from: '${process.env.EMAIL_USER}',
+  //       to: 'brunovolpatol@hotmail.com',
+  //       subject: 'Welcome to NestJS',
+  //       text: 'Welcome to NestJS',
+  //     };
+
+  //     const result = await service.sendEmail(options);
+
+  //     expect(result).toEqual({ success: true });
+  //   });
+  // });
 });
